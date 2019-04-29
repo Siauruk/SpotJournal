fdc6e9ce8ff6cabab99a77fa4cb6d2b1a6408e7a
Anton Siauruk <siauruk@icloud.com>
2019-04-29 12:14:03 +0300

    Create TableViewController

---
+++ SpotJournal/MainTableViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MainTableViewController.swift
+//  SpotJournal
+//
+//  Created by Anton Siauruk on 4/29/19.
+//  Copyright © 2019 Anton Siauruk. All rights reserved.
+//
+
+import UIKit
+
+class MainTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        return cell
+    }
+}

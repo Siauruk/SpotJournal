049a98919686012bf853fd32285edb6081729d68
Anton Siauruk <siauruk@icloud.com>
2019-04-29 12:49:43 +0300

    Configure the Cell

--- SpotJournal/MainTableViewController.swift
+++ SpotJournal/MainTableViewController.swift
@@ -9,26 +9,33 @@
 import UIKit
 
 class MainTableViewController: UITableViewController {
+    
+    let tempRestaurantNames = [
+        "Katz’s Delicatessen", "Peter Luger", "Lombardi’s",
+        "Keens Steakhouse", "Junior’s Restaurant", "Tavern on the Green",
+        "The Rainbow Room", "Totonno's", "The Russian Tea Room",
+        "Delmonico’s", "Rao’s", "Grand Central Oyster Bar & Restaurant",
+        "Nathan's Famous", "Minetta Tavern", "The Odeon", "Bamonte’s",
+        "21 Club", "John’s Pizzeria", "La Grenouille", "P.J. Clarke’s",
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
+        
     }
 
     
     // MARK: - Table view data source
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return tempRestaurantNames.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        cell.textLabel?.text = tempRestaurantNames[indexPath.row]
 
         return cell
     }

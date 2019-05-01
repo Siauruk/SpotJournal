01185cb7687d7a4bd81d9849a0baaeeb13c7d68c
Anton Siauruk <siauruk@icloud.com>
2019-05-01 11:56:51 +0300

    Design the App

--- SpotJournal/MainTableViewController.swift
+++ SpotJournal/MainTableViewController.swift
@@ -14,9 +14,9 @@ class MainTableViewController: UITableViewController {
         "Katz’s Delicatessen", "Peter Luger", "Lombardi’s",
         "Keens Steakhouse", "Junior’s Restaurant", "Tavern on the Green",
         "The Rainbow Room", "Totonno's", "The Russian Tea Room",
-        "Delmonico’s", "Rao’s", "Grand Central Oyster Bar & Restaurant",
+        "Delmonico’s", "Rao’s", "P.J. Clarke’s", "La Grenouille",
         "Nathan's Famous", "Minetta Tavern", "The Odeon", "Bamonte’s",
-        "21 Club", "John’s Pizzeria", "La Grenouille", "P.J. Clarke’s",
+        "21 Club", "John’s Pizzeria"
     ]
 
     override func viewDidLoad() {
@@ -37,7 +37,15 @@ class MainTableViewController: UITableViewController {
         
         cell.textLabel?.text = tempRestaurantNames[indexPath.row]
         cell.imageView?.image = UIImage(named: tempRestaurantNames[indexPath.row])
+        cell.imageView?.layer.cornerRadius = cell.frame.height / 2
+        cell.imageView?.clipsToBounds = true
 
         return cell
+    }
+    
+    // MARK: - Table view delegate
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 85
     }
 }

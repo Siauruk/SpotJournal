39ec0103a2b1675499414d11e0acbbfb0f591894
Anton Siauruk <siauruk@icloud.com>
2019-05-01 12:47:01 +0300

    Custom Table View Cell

---
+++ SpotJournal/CustomTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CustomTableViewCell.swift
+//  SpotJournal
+//
+//  Created by Anton Siauruk on 5/1/19.
+//  Copyright © 2019 Anton Siauruk. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    
+}
--- SpotJournal/MainTableViewController.swift
+++ SpotJournal/MainTableViewController.swift
@@ -33,12 +33,12 @@ class MainTableViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! CustomTableViewCell
         
-        cell.textLabel?.text = tempRestaurantNames[indexPath.row]
-        cell.imageView?.image = UIImage(named: tempRestaurantNames[indexPath.row])
-        cell.imageView?.layer.cornerRadius = cell.frame.height / 2
-        cell.imageView?.clipsToBounds = true
+        cell.nameLabel?.text = tempRestaurantNames[indexPath.row]
+        cell.imageOfPlace?.image = UIImage(named: tempRestaurantNames[indexPath.row])
+        cell.imageOfPlace?.layer.cornerRadius = cell.imageOfPlace.frame.size.height / 2
+        cell.imageOfPlace?.clipsToBounds = true
 
         return cell
     }

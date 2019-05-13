c6c54473750c616c6f21c6db2967d620f58b7030
Anton Siauruk <siauruk@icloud.com>
2019-05-13 11:26:29 +0300

    Map View Controller

---
+++ SpotJournal/MapViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MapViewController.swift
+//  SpotJournal
+//
+//  Created by Anton Siauruk on 5/13/19.
+//  Copyright © 2019 Anton Siauruk. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    @IBAction func closeVC() {
+        dismiss(animated: true)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
